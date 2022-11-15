a0796b58da1e18c1ecd7bff7b66e2ec7deea742a
RomMyk <baron1xxx@gmail.com>
2022-11-15 10:11:01 +0200

    Init styles

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Hello React Native</Text>
     </View>
   );
 }
@@ -13,8 +11,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#fff',
+    fontSize: 28
+  }
 });
